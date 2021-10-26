2afc09753a1429dcaf81bb7a35142f00524f9f38
preethi0899 <93186537+preethi0899@users.noreply.github.com>
2021-10-26 17:20:12 +0530

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -15,22 +15,19 @@ exports.index = function(req, res) {
     });
 };
 
-
-// Display list of all books.
 exports.products_list = function(req, res, next) {
  
   Products .find({}, 'products_id')
     .populate('product_name').exec(function (err, list_products) {
       if (err) {return next(err)} 
       else {
-            // Successful, so render
+      
             res.render('products_list', {title: 'Products List', products_list:  list_products});
         }
     });
 
 };
 
-// Display detail page for a specific book.
 exports.products_detail = function(req, res, next) {
 
     async.parallel({
@@ -38,12 +35,12 @@ exports.products_detail = function(req, res, next) {
 
             Products.findById(req.params.id)
               .populate('product_name')
-              .populate('seller_id').populate(type)
+              .populate('seller_id')..populate('cost').populate('type')
               .exec(callback);
         }
          products_instance: function(callback) {
 
-          ProductInstance.find({ 'book': req.params.id })
+          ProductInstance.find({ 'products': req.params.id })
           .exec(callback);
         },
     }, function(err, results) {
@@ -53,8 +50,8 @@ exports.products_detail = function(req, res, next) {
             err.status = 404;
             return next(err);
         }
-        // Successful, so render.
-        res.render('products_detail', { title: results.products.title, product:  results.product, products_instances: results.products_instance } );
+     
+        res.render('products_detail', { product_name: results.products.product_name, product:  results.product, products_instances: results.products_instance } );
     });
 
 };
